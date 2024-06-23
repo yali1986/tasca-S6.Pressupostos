280c02a4facb6c196fc5546c7951ebfca21cdaaa
Yalina Gómez <yalinita1985@gmail.com>
2024-06-23 16:15:33 +0200

    layout responsive Pages_and_Languages

--- src/components/Pages_and_Lenguages.jsx
+++ src/components/Pages_and_Lenguages.jsx
@@ -5,38 +5,44 @@ import { useState } from "react"
 export default function Pages_and_Lenguages({ pages, languages, onCounterChange }) {
 
     return (
-        <>
-          <div className="col">
-            <div className="row mb-2">
-              <div className="col fw-medium fs-6">Nombre de pàgines</div>
-              <div className="col d-flex align-items-center">
+        <>               
+
+            <div className="row mb-2 row text-start text-end-lg">
+              <div className="col-12 col-md-8 d-flex align-items-center fw-medium fs-6 justify-content-center justify-content-md-end">Nombre de pàgines</div>
+
+              <div className="col-12 col-md-4 pe-xl-5 d-flex align-items-center justify-content-center justify-content-md-end">
                 <button 
                   className="btn btn-outline-secondary rounded-circle me-2"
                   onClick={() => onCounterChange('pages', -1)}
                 >-</button>
-                <div>{pages}</div>
+                <div className="nombre">{pages}</div>
                 <button 
                   className="btn btn-outline-secondary rounded-circle ms-2"
                   onClick={() => onCounterChange('pages', 1)}
                 >+</button>
               </div>
             </div>
+
+
     
-            <div className="row">
-              <div className="col fw-medium fs-6">Nombre de llenguatges</div>
-              <div className="col d-flex align-items-center">
+            <div className="row text-start text-end-lg">
+              <div className="col-12 col-md-8 d-flex align-items-center fw-medium fs-6 justify-content-center justify-content-md-end">Nombre de llenguatges</div>
+
+
+              <div className="col-12 col-md-4 pe-xl-5 d-flex align-items-center justify-content-center justify-content-lg-end">
                 <button 
                   className="btn btn-outline-secondary rounded-circle me-2"
                   onClick={() => onCounterChange('languages', -1)}
                 >-</button>
-                <div>{languages}</div>
+                <div className="nombre">{languages}</div>
                 <button 
                   className="btn btn-outline-secondary rounded-circle ms-2"
                   onClick={() => onCounterChange('languages', 1)}
                 >+</button>
               </div>
-            </div>
-          </div>
+
+            </div>         
+       
         </>
       )
 }
